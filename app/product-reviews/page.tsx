0d6b976aa4d661b8f860e748e183aab0e5317098
AKLIGO <koffitsejeromea@gmail.com>
2026-03-27 17:31:58 +0000

    Utilisation de Suspense

--- app/product-reviews/page.tsx
+++ app/product-reviews/page.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
+
 import { Product } from "@/components/product";
 import { Reviews } from "@/components/reviews";
 export default function ProductReviews() {
     return (
         <>
             <h1>Product Reviews page</h1>
-            <Product />
-            <Reviews />
+            <Suspense fallback={<div>Loading product details...</div>}>
+                <Product />
+            </Suspense>
+            
+            <Suspense fallback={<div>Loading reviews...</div>}>
+                <Reviews />
+            </Suspense>
         </>
     );
 }
